b5d10759ad9b58b335525f5fded6df5cbc27af76
Irisviel22 <58122019+Irisviel22@users.noreply.github.com>
2023-11-07 17:43:40 +0100

    卡片显示修改

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -19,53 +19,100 @@ function changeIconImgSrc(src) {
 	document.getElementById("theme-toggle-img--mobile").src = src;
 }
 
-if (systemInitiatedDark.matches) {
-	changeIconImgSrc(iconMoon);
-} else {
-	changeIconImgSrc(iconSun);
-}
+// new
 
-function prefersColorTest(systemInitiatedDark) {
-  if (systemInitiatedDark.matches) {
-  	document.documentElement.setAttribute('data-theme', 'dark');		
-   	changeIconImgSrc(iconMoon);
-   	sessionStorage.setItem('theme', '');
-  } else {
-  	document.documentElement.setAttribute('data-theme', 'light');
-    changeIconImgSrc(iconSun);
-    sessionStorage.setItem('theme', '');
-  }
-}
-systemInitiatedDark.addListener(prefersColorTest);
-
-
-function modeSwitcher() {
-	let theme = sessionStorage.getItem('theme');
+function setTheme(theme) {
 	if (theme === "dark") {
-		document.documentElement.setAttribute('data-theme', 'light');
-		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc(iconSun);
-	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		changeIconImgSrc(iconMoon);
-	} else if (systemInitiatedDark.matches) {	
-		document.documentElement.setAttribute('data-theme', 'light');
-		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc(iconSun);
 	} else {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc(iconMoon);
+		document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+		changeIconImgSrc(iconSun);
 	}
 }
 
-if (theme === "dark") {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	sessionStorage.setItem('theme', 'dark');
-	changeIconImgSrc(iconMoon);
-} else if (theme === "light") {
-	document.documentElement.setAttribute('data-theme', 'light');
-	sessionStorage.setItem('theme', 'light');
-	changeIconImgSrc(iconSun);
+if (theme) {
+	setTheme(theme); // 如果用户已经选择了主题，应用用户选择的主题
+} else if (systemInitiatedDark.matches) {
+	setTheme("dark"); // 如果用户没有选择主题但系统偏好暗色模式，则应用夜间模式
+} else {
+	setTheme("light"); // 默认应用日间模式
+}
+
+// old
+
+// if (systemInitiatedDark.matches) {
+// 	changeIconImgSrc(iconMoon);
+// } else {
+// 	changeIconImgSrc(iconSun);
+// }
+
+// function prefersColorTest(systemInitiatedDark) {
+//   if (systemInitiatedDark.matches) {
+//   	document.documentElement.setAttribute('data-theme', 'dark');		
+//    	changeIconImgSrc(iconMoon);
+//    	sessionStorage.setItem('theme', '');
+//   } else {
+//   	document.documentElement.setAttribute('data-theme', 'light');
+//     changeIconImgSrc(iconSun);
+//     sessionStorage.setItem('theme', '');
+//   }
+// }
+
+// new
+
+function prefersColorTest(systemInitiatedDark) {
+	if (systemInitiatedDark.matches) {
+		setTheme("dark");
+	} else {
+		setTheme("light");
+	}
+}
+
+systemInitiatedDark.addListener(prefersColorTest);
+
+//old
+
+// function modeSwitcher() {
+// 	let theme = sessionStorage.getItem('theme');
+// 	if (theme === "dark") {
+// 		document.documentElement.setAttribute('data-theme', 'light');
+// 		sessionStorage.setItem('theme', 'light');
+// 		changeIconImgSrc(iconSun);
+// 	}	else if (theme === "light") {
+// 		document.documentElement.setAttribute('data-theme', 'dark');
+// 		sessionStorage.setItem('theme', 'dark');
+// 		changeIconImgSrc(iconMoon);
+// 	} else if (systemInitiatedDark.matches) {	
+// 		document.documentElement.setAttribute('data-theme', 'light');
+// 		sessionStorage.setItem('theme', 'light');
+// 		changeIconImgSrc(iconSun);
+// 	} else {
+// 		document.documentElement.setAttribute('data-theme', 'dark');
+// 		sessionStorage.setItem('theme', 'dark');
+// 		changeIconImgSrc(iconMoon);
+// 	}
+// }
+
+// if (theme === "dark") {
+// 	document.documentElement.setAttribute('data-theme', 'dark');
+// 	sessionStorage.setItem('theme', 'dark');
+// 	changeIconImgSrc(iconMoon);
+// } else if (theme === "light") {
+// 	document.documentElement.setAttribute('data-theme', 'light');
+// 	sessionStorage.setItem('theme', 'light');
+// 	changeIconImgSrc(iconSun);
+// }
+
+
+// new
+function modeSwitcher() {
+	let currentTheme = sessionStorage.getItem('theme');
+	if (currentTheme === "dark") {
+		setTheme("light");
+	} else {
+		setTheme("dark");
+	}
 }
